7a0ed1b840ee3a3a742416d81cc078ebc8c6052d
rima1881 <amd.gharibipour@outlook.com>
2025-07-20 21:50:51 -0400

    update websocket connection

--- websocketTest/test.py
+++ websocketTest/test.py
@@ -4,8 +4,9 @@ import websockets
 async def connect():
     uri = "ws://localhost:8080"
     async with websockets.connect(uri) as websocket:
-        await websocket.send("Hello from Python (async)")
-        response = await websocket.recv()
-        print("Received:", response)
+        while True:
+            response = await websocket.recv()
+            print("Received:", response)
+
 
 asyncio.run(connect())
