9febce39cf58dc1ebb4f11807faf4869fe9439c5
Tristan Ovesson <tristan.ovesson@gmail.com>
2025-08-05 11:30:24 -0400

    metroNameScrape image update

--- helpers/metroNameScrape.mjs
+++ helpers/metroNameScrape.mjs
@@ -43,8 +43,8 @@ async function metroNameScrape(searchName) {
       .catch(() => 'No barcode');
 
     const img = await product
-      .$eval('img', img => img.getAttribute('src'))
-      .catch(() => 'No image');
+      .$eval('picture.defaultable-picture img', img => img.getAttribute('src'))
+      .catch(() => 'No image'); // If no image is found, return 'No image'
 
     const title = await product
       .$eval('.head__title', el => el.innerText.trim())
